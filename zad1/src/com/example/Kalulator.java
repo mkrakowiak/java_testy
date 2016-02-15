72577655a20330510a53a7bf908a5b84988beab4
Michał Krakowiak <mkrakowiak@p29-3.labpk.inf.ug.edu.pl>
2016-02-15 14:00:24 +0100

    zad1

---
+++ zad1/src/com/example/Kalulator.java
@@ -0,0 +1,32 @@
+package com.example;
+
+
+
+public class Kalulator {
+	
+	public Kalulator(){
+		System.out.println("Kalkulator" + this);
+		
+		
+	}
+	
+	public int add(int n1, int n2){
+		return n1 + n2;
+	}
+	
+	public int sub(int n1, int n2){
+		return n1 - n2;
+	}
+	int multi(int a,int b){
+		return a*b;
+	}
+	int div(int a, int b){
+		return a/b;
+	}
+	
+	boolean greater(int a, int b){
+		if(a>b) return true;
+		else return false;
+	}
+	
+}
