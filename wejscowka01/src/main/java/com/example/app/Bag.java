cdc48865ef3d9cac982a23e10e2912eba5f3ef94
Michał Krakowiak <mkrakowiak@p29-3.labpk.inf.ug.edu.pl>
2016-02-29 12:55:39 +0100

    zmiana

--- wejscowka01/src/main/java/com/example/app/Bag.java
+++ wejscowka01/src/main/java/com/example/app/Bag.java
@@ -1,11 +1,18 @@
 package com.example.app;
 
 public class Bags {
-	
+	public int aa;
+	public double bb
+	public float cc;
 
 }
 public class BagManager {
 	
-	
-	
+	public int add(int n1, int n2){
+		return n1 + n2;
+	}
+	public int sub(int n1, int n2){
+		
+		return n1 - n2;
+	}
 }
--- wejscowka01/src/test/java/com/example/app/BagTest.java
+++ wejscowka01/src/test/java/com/example/app/BagTest.java
@@ -3,9 +3,21 @@ package com.example.app;
 import static org.junit.Assert.*;
 import org.junit.rules.ExpectedException;
 
-import com.example.app.Calculator;
+import com.example.app.Bag;
 
-public class CalculatorTest {
+public class BagTest {
 	
+	
+	private Bag bagg = new Bag();
+	@Test
+	public void addCheck(){	
+		assertEquals(5, bagg.add(3,2));
+		
+	}
+	@Test
+	public void addSub(){
+		assertEquals(1, bagg.sub(3,2));
+		
+	}
 
 }
