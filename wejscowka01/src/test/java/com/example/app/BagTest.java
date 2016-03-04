0e591b39785ded186b06848dfeff823cbfc870e9
Michał Krakowiak <mkrakowiak@sigma.ug.edu.pl>
2016-03-04 17:04:56 +0100

    dodanie testu usun

--- wejscowka01/src/test/java/com/example/app/BagTest.java
+++ wejscowka01/src/test/java/com/example/app/BagTest.java
@@ -25,5 +25,25 @@ public class BagTest {
 
 		
 	}
-	
+	@Test
+	public void checkRemoving(){
+
+		int SizeBeforeAdd = 0;
+
+		Bag cos = new Bag(7,(float)8.1,9.3);
+		bagManager.addBag(cos);
+		
+		int SizeAfterAdd = bagManager.bags.size();
+
+		int Sub = SizeAfterAdd - SizeBeforeAdd;
+
+		assertEquals(1, Sub);
+
+		bagManager.removeBag(cos);
+
+		int SizeAfterRemove = bagManager.bags.size();
+
+		assertEquals(0, SizeAfterRemove);
+
+	}
 }
