dffb511dc1f9e904ff9176208c807463d04809b7
Michał Krakowiak <mkrakowiak@p29-3.labpk.inf.ug.edu.pl>
2016-04-18 13:57:03 +0200

    add remove

--- wejscowka02/src/test/java/com/example/app/BagManagerSteps.java
+++ wejscowka02/src/test/java/com/example/app/BagManagerSteps.java
@@ -33,6 +33,29 @@ public class BagManagerSteps {
 	public void shouldAdd(int result){
     	assertEquals(1, result);
 	}
+	
+	@When("remove from list when weight is $weight is $air true width $width")
+	public void removeArguments(int weight, boolean air, double width){
+		int SizeBeforeAdd = 0;
+		BagManager bagManager = new BagManager();
+		Bag cos = new Bag(weight,air,width);
+		bagManager.addBag(cos);
+		
+		int SizeAfterAdd = bagManager.bags.size();
+
+		int Sub = SizeAfterAdd - SizeBeforeAdd;
+
+		assertEquals(1, Sub);
+
+		bagManager.removeBag(cos);
+
+		int SizeAfterRemove = bagManager.bags.size();
+	}
+	
+    @Then("remove should return size $result")
+	public void shouldRemove(int result){
+    	assertEquals(0, result);
+	}
     
 }
 
